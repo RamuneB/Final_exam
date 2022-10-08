493b3e80f8f2a1a14203139451f1175d3d0d870e
Ramunė Butauskienė <101128898+RamuneB@users.noreply.github.com>
2022-10-08 11:11:09 +0300

    add all

--- django_demo/library/models.py
+++ django_demo/library/models.py
@@ -9,7 +9,6 @@ from django.contrib.auth.models import User
 
 
 class Uzrasas(models.Model):
-    """Modelis reprezentuoja knygą (bet ne specifinę knygos kopiją)"""
     title = models.CharField('Pavadinimas', max_length=200)
     kategorija = models.ForeignKey('Kategorija', on_delete=models.SET_NULL, null=True, related_name='uzrasai')
     summary = models.TextField('Tekstas', max_length=1000)
@@ -23,8 +22,7 @@ class Uzrasas(models.Model):
         verbose_name = 'Užrašas'
         verbose_name_plural = 'Užrašai'
 
-   # def display_genre(self):
-    #    return ', '.join(genre.name for genre in self.genre.all()[:3])
+  
 
     def kategorija_name(self):
         if self.kategorija:
@@ -32,37 +30,9 @@ class Uzrasas(models.Model):
         else:
             return '-'
 
-    #display_genre.short_description = 'Žanras'
+   
 
-'''
-class UzrasasInstance(models.Model):
-    """Modelis, aprašantis konkrečios knygos kopijos būseną"""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unikalus ID knygos kopijai')
-    uzrasas = models.ForeignKey('Uzrasas', on_delete=models.SET_NULL, null=True) 
-    due_back = models.DateField('Bus prieinama', null=True, blank=True)
 
-    LOAN_STATUS = (
-        ('a', 'Administruojama'),
-        ('p', 'Paimta'),
-        ('g', 'Galima paimti'),
-        ('r', 'Rezervuota'),
-    )
-
-    status = models.CharField(
-        max_length=1,
-        choices=LOAN_STATUS,
-        blank=True,
-        default='a',
-        help_text='Statusas',
-    )
-
-    class Meta:
-        ordering = ['due_back']
-
-    def __str__(self):
-        return f'{self.id} ({self.uzrasas.title})'
-
-'''
 class Kategorija(models.Model):
    
     first_name = models.CharField('Vardas', max_length=100)
@@ -77,13 +47,6 @@ class Kategorija(models.Model):
 
     display_uzrasai.short_description = 'Užrašai'
 
-'''
-class Genre(models.Model):
-    name = models.CharField('Pavadinimas', max_length=200)
-
-    def __str__(self):
-        return self.name
-'''
 
 class Profilis(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
